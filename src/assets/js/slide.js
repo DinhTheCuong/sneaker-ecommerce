a2406b4dda6bc199861620c84a1436b8be67fbc6
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-02-01 19:53:25 +0700

    update4

--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -1,22 +1,22 @@
 // Next/Prev buttons
 
 // next
-var nextBtn = document.getElementsByClassName('next-btn');
+// var nextBtn = document.getElementsByClassName('next-btn');
 
-function slideRight() {
-    var slider = document.getElementById('slider');
-    // alert('Slide');
-    console.log(slider)
-    slider.scrollLeft += 300;
-};
+// function slideRight() {
+//     var slider = document.getElementById('slider');
+//     // alert('Slide');
+//     console.log(slider)
+//     slider.scrollLeft += 300;
+// };
 
 
-// prev
-var prevBtn = document.getElementsByClassName('prev-btn');
+// // prev
+// var prevBtn = document.getElementsByClassName('prev-btn');
 
-function slideLeft() {
-    var slider = document.getElementById('slider');
-    // alert('Slide');
-    console.log(slider)
-    slider.scrollLeft -= 300;
-};
+// function slideLeft() {
+//     var slider = document.getElementById('slider');
+//     // alert('Slide');
+//     console.log(slider)
+//     slider.scrollLeft -= 300;
+// };
